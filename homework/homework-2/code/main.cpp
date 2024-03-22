ae01459827916a50cf2bc84fa3397b1891736a5f
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2024-03-21 19:52:32 -0700

    progress

--- homework/homework-2/code/main.cpp
+++ homework/homework-2/code/main.cpp
@@ -1,7 +1,18 @@
 #include "node.hpp"
 #include <iostream>
+#include <iterator>
+#include <ostream>
+#include <queue>
 #include <string>
+#include <utility>
 #include <vector>
+
+using std::cout;
+using std::endl;
+using std::pair;
+using std::vector;
+
+// typedef vector<pair<Node *, int>> stateSpace;
 
 // This class will operate on the nodes that are passed to it to calculate the best path to a solution.
 class a_star
@@ -15,35 +26,181 @@ public:
 
   void printRoot() { rootNode->PrintNode(); }
 
-  void run()
+  // runs the a-star and returns a pointer to the solution node.
+  Node *run()
   {
 
-    // create a node to begin testing
+    // Begin testing at the root.
     Node *currNode = this->rootNode;
 
+    Node *testNode = new Node(*currNode);
+
+    std::queue<Node *> searchQueue;
+    searchQueue.push(currNode);
+
+    currNode->addChild(testNode);
+    testNode->setParent(currNode);
+    testNode->moveTile(4, 1);
+
+    // add the first node
+    searchQueue.push(currNode);
+
     // begin finding a solution
-    while (currNode->getCost() != 0)
+    while (currNode->getBoardCost() != 0)
     {
 
+      // return the node if a solution is found.
+      if (currNode->getBoardCost() == 0)
+      {
+        cout << "Goal Reached! Printing Solution Now" << endl;
+        return currNode;
+      }
 
+      // generate all of the children of the current state.
+      vector possibleMoves = generateChildMoves(currNode);
 
+      // clang-format off
+      // DEBUG
+      
+      cout << "Before removing Duplicates" << endl; 
+      for (auto move : possibleMoves) { cout << move->getBoard() << endl; }
 
+      // Remove duplicate nodes.
+      removeDuplicates(possibleMoves);
+
+      // DEBUG
+      cout << "After removing Duplicates" << endl; for (auto move : possibleMoves) { cout << move->getBoard() << endl; }
+      std::string temp; cout << "Waiting on input from user" << endl; std::cin >> temp;
+      // clang-format on
     }
+
+    // returns the
+    return currNode;
   };
+
+  // searches for a duplicate nodes in a given vector and removes them.
+  void removeDuplicates(vector<Node *> possibleMoves)
+  {
+    // delete all duplicate nodes that already exist.
+    for (int index = 0; index < (int)possibleMoves.size(); index++)
+    {
+
+      // looks for a node in the tree.
+      if (searchForNode(possibleMoves.at(index)))
+      {
+        cout << "Duplicate Node Found: " << possibleMoves.at(index)->getBoard() << endl;
+        possibleMoves.erase(possibleMoves.begin(), possibleMoves.begin() + index);
+      }
+    }
+  }
+
+  // method to search for a node.
+  Node *searchForNode(Node *input)
+  {
+    vector<Node *> searchQueue;
+    searchQueue.push_back(this->rootNode);
+
+    // begin searching the queue.
+    while (!searchQueue.empty())
+    {
+      int queueSize = searchQueue.size();
+
+      while (queueSize > 0)
+      {
+        // get the first node
+        Node *currentNode = searchQueue.front();
+
+        // remove the node from the queue.
+        searchQueue.erase(searchQueue.begin());
+
+        // return matching node
+        if (currentNode->getBoard() == input->getBoard())
+        {
+          cout << "Matching Board: " << currentNode->getBoard() << endl;
+          return currentNode;
+        }
+
+        // Append children of the queued item.
+        for (int i = 0; i < (int)currentNode->getChildren().size(); i++)
+        {
+          searchQueue.push_back(currentNode->getChildren()[i]);
+          queueSize++;
+        }
+
+        queueSize--;
+      }
+    }
+
+    return nullptr;
+  }
+
+  void printChilren(vector<Node *> input)
+  {
+
+    // print all found moves.
+    for (auto child : input)
+    {
+      child->printBoard();
+    }
+  }
+
+  vector<Node *> generateChildMoves(Node *currNode)
+  {
+    vector<Node *> possibleMoves;
+
+    for (int tile = 0; tile < 6; tile++)
+    {
+
+      // iterate through all possible moves.
+      for (int move = 0; move < 5; move++)
+      {
+        Node *newMove = new Node(*currNode);
+
+        int moveCost = newMove->moveTile(tile, move);
+
+        if (moveCost > 0)
+        {
+          newMove->incrementNodeCost(moveCost);
+          possibleMoves.push_back(newMove);
+          cout << "Move Generated from: " + currNode->getBoard() + " : " + newMove->getBoard() << endl;
+          cout << "New Move Board Cost: " << newMove->getBoardCost() << " New Move Node Cost: " << newMove->getNodeCost() << endl;
+        }
+        else
+        {
+          delete newMove;
+        }
+      }
+    }
+
+    return possibleMoves;
+  }
 
   void showOptimalPath(){
 
   };
 
+  // This function generates a vector given a solution node.
+  // The vector recurses back from to the parent node.
+  vector<Node *> generateSolution(Node *) {}
+
+  void printSolution(vector<Node *> solution)
+  {
+
+    for (int i = solution.size(); i >= 0; i--)
+    {
+      Node *curr = solution.at(i);
+      cout << "Cost: " << curr->getBoardCost() << "\t Board:" << curr->getBoard() << endl;
+    }
+  }
+
 private:
   Node *rootNode;
-  std::vector<std::vector<Node *>> levels;
 };
 
 int main(int argc, char *argv[])
 {
 
-  std::string firstState = "bbb_www";
+  std::string firstState = "bbbwww_";
 
   if (argc == 2)
   {
@@ -51,16 +208,16 @@ int main(int argc, char *argv[])
 
     if (input.size() != 7)
     {
-      std::cout << "Size of the board should be 7.\n Your string has size: " << input.size() << std::endl;
+      cout << "Size of the board should be 7.\n Your string has size: " << input.size() << endl;
       exit(-1);
     }
 
-    // std::cout << "Initial State: " << firstState << std::endl;
+    // cout << "Initial State: " << firstState << endl;
     firstState = input;
   }
   else if (argc > 2)
   {
-    std::cout << "Usage: {program} {initial state}" << std::endl;
+    cout << "Usage: {program} {initial state}" << endl;
   }
 
   // Initialize the node.
@@ -68,7 +225,7 @@ int main(int argc, char *argv[])
 
   a_star myAStar(initialNode);
 
-  initialNode->PrintNode();
+  // initialNode->PrintNode();
 
   myAStar.run();
 
--- homework/homework-2/code/node.hpp
+++ homework/homework-2/code/node.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -8,38 +9,294 @@ class Node
 {
 public:
   // Constructors
-  Node(){
+  Node()
+  {
     this->board = std::string(7, 'x');
     updateBoardCost();
+  };
 
-  };
-  Node(std::string input) : board(input){
-    updateBoardCost();
-  };
-  Node(Node const &input);
+  // Given the string
+  Node(std::string input) : board(input) { updateBoardCost(); };
+
+  // Copy Constructor
+  Node(const Node &input)
+  {
+    this->parent = input.parent;
+    this->board = input.board;
+    this->boardCost = input.boardCost;
+    this->children = input.children;
+    this->cumulativeMoveCost = input.cumulativeMoveCost;
+  }
+
+  // Destructor
   ~Node(){};
 
   // Print the current node board.
-  void PrintNode();
+  void PrintNode()
+  {
+    std::cout << "Board Cost: " << this->boardCost << std::endl;
+    for (auto &letter : this->board)
+    {
+      std::cout << letter;
+    }
+
+    std::cout << std::endl;
+  }
 
   // calculate the total cost of the current board and assign the cost to the `boardCost` variable.
-  void updateBoardCost();
+  void updateBoardCost()
+  {
+
+    int blackTiles = 0;
+    int cost = 0;
+    for (auto &letter : board)
+    {
+      if (letter == 'b')
+      {
+        blackTiles++;
+      }
+
+      if (letter == 'w')
+      {
+        cost += blackTiles;
+      }
+    }
+
+    this->boardCost = cost;
+  }
 
   // set the new board state using a string.
-  void setBoard(std::string input);
+  void setBoard(std::string input)
+  {
+
+    if (input.size() != 7)
+    {
+      std::cout << "Board size is smaller than 7 tiles" << std::endl;
+      exit(-1);
+    }
+
+    this->board = input;
+
+    updateBoardCost();
+  }
 
   // pick a tile and make it move where you want.
-  int moveTile(int tile, int move);
+  // Move the tile to a valid spot.
+  bool validMove(int tileIndex, int move)
+  {
+    if (tileIndex < 0 || tileIndex >= 7)
+    {
+      std::cout << "Invalid tile Size used to move tile. Tile Size: " << tileIndex << std::endl;
+      return false;
+    }
 
-  int getCost();
+    // checking move and making the move if it is valid.
+    if (move == 0)
+    {
 
-  //
-  // void operator=(Node const &obj);
+      int change = tileIndex + 3;
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot hop over two places to the right." << std::endl;
+        return false;
+      }
+    }
+    else if (move == 1)
+    {
+
+      int change = tileIndex + 2;
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot hop over a tile to the right." << std::endl;
+        return false;
+      }
+    }
+    else if (move == 2)
+    {
+      int change = tileIndex + 1;
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot move adjacent to the right." << std::endl;
+        return false;
+      }
+    }
+    else if (move == 3)
+    {
+      int change = tileIndex - 1;
+
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot adjacent to the left." << std::endl;
+        return false;
+      }
+    }
+    else if (move == 4)
+    {
+      int change = tileIndex - 2;
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot hop over a tile to the left." << std::endl;
+        return false;
+      }
+    }
+    else if (move == 5)
+    {
+
+      int change = tileIndex - 3;
+      if (change >= 7 || change < 0)
+      {
+        // std::cout << "The move is out of bounds" << std::endl;
+        return false;
+      }
+      if (this->board.at(change) != '_')
+      {
+        // std::cout << "Tile " << tileIndex << " cannot hop over two tiles to the left." << std::endl;
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  int condutcMoveOperation(int tileIndex, int move)
+  {
+
+    // there are 6 possible moves
+    // 0. Tile hops two spots to the right.
+    // 1. Tile hops over one tile to the right.
+    // 2. Tile slides to the adjacent right.
+    // 3. Tile moves to the adjacent left.
+    // 4. Tile hops over one tile to the left.
+    // 5. Tile hops over two tiles to the left.
+
+    // making the move if it is valid.
+    int cost;
+
+    if (move == 0)
+    {
+      int change = tileIndex + 3;
+      cost = 3;
+
+      char tempTile = this->board.at(tileIndex);
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+    else if (move == 1)
+    {
+      cost = 2;
+
+      int change = tileIndex + 2;
+      char tempTile = this->board.at(tileIndex);
+
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+    else if (move == 2)
+    {
+      int change = tileIndex + 1;
+      cost = 1;
+      char tempTile = this->board.at(tileIndex);
+
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+    else if (move == 3)
+    {
+      int change = tileIndex - 1;
+      cost = 1;
+
+      char tempTile = this->board.at(tileIndex);
+
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+    else if (move == 4)
+    {
+      int change = tileIndex - 2;
+      cost = 2;
+      char tempTile = this->board.at(tileIndex);
+
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+    else if (move == 5)
+    {
+
+      int change = tileIndex - 3;
+      cost = 3;
+      char tempTile = this->board.at(tileIndex);
+
+      this->board.at(tileIndex) = this->board.at(change);
+      this->board.at(change) = tempTile;
+    }
+
+    updateBoardCost();
+
+    return cost;
+  }
+
+  void incrementNodeCost(int cost) { this->cumulativeMoveCost += cost; }
+
+  // moves the tile by switching the string returns the cost.
+  int moveTile(int tileIndex, int move)
+  {
+    if (validMove(tileIndex, move))
+    {
+      return condutcMoveOperation(tileIndex, move);
+    }
+    return -1;
+  }
+
+  void printBoard() { std::cout << this->board << std::endl; }
+
+  /*
+   * Getters
+   */
+  int getBoardCost() { return this->boardCost; }
+  int getNodeCost() { return this->cumulativeMoveCost; }
+  std::string getBoard() { return this->board; }
+  Node *getParent() { return this->parent; }
+  std::vector<Node *> getChildren() { return this->children; }
+  void addChild(Node *newChild) { this->children.push_back(newChild); }
+  void setParent(Node *newParent) { this->parent = newParent; }
 
 private:
+  // Stores the state of the board.
   std::string board;
+
+  // Stores the cost of the node's board.
   int boardCost;
 
+  // stores the cost of the moves required to get to the current board state.
+  int cumulativeMoveCost; // TODO still need to implement the node cost features.
+
+  // Stores the parent of the node.
   Node *parent;
+
   std::vector<Node *> children;
 };
